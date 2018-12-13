e18a0a4b9d28fa4010a32569ff5400a43e98c715
Nayra-Adel <nayra.adel128@stud.fci-cu.edu.eg>
2018-12-13 18:43:28 +0200

    Added Sigmoid Fn "Activation Fn"

--- Neural Network.py
+++ Neural Network.py
@@ -5,6 +5,10 @@ def calculate_mse(delta):
     return np.sum(delta ** 2, axis=0) / delta.shape[0]
 
 
+def sigmoid(net):
+    return 1 / (1 + np.exp(-net))
+
+
 class NeuralNetwork:
     def __init__(self, m, l, n, input_vector):
         self.hidden_weights = np.random.rand(m, l) * 10 + (-5)  # m*L
@@ -15,8 +19,8 @@ class NeuralNetwork:
 
     # biases to be 0 [Wx+b] => (b = 0)
     def feed_forward(self):
-        self.hidden_layers = np.dot(self.input_layers, self.hidden_weights)  # (k*m).(m*L)
-        output_layers = np.dot(self.hidden_layers, self.output_weights)  # (k*L).(L*n)
+        self.hidden_layers = sigmoid(np.dot(self.input_layers, self.hidden_weights))  # (k*m).(m*L)
+        output_layers = sigmoid(np.dot(self.hidden_layers, self.output_weights))  # (k*L).(L*n)
         return output_layers  # return the normalized output
 
 
